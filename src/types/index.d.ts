555fc989b06c2bf0315acbfe2b2e81870efbe012
Babita00 <babitaasth07@gmail.com>
2026-03-04 18:56:40 +0545

    refactor: streamline type definitions in index.d.ts for improved readability

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,34 +1,34 @@
-export type Role = 'super_admin' | 'artist_manager' | 'artist';
-export type Gender = 'M' | 'F' | 'O';
+import { Gender } from './gender'
+import { Role } from './userRole'
 export interface AppBaseEntity {
-    id: string;
-    created_at: Date;
-    updated_at: Date;
-    deleted_at?: Date | null;
+  id: string
+  created_at: Date
+  updated_at: Date
+  deleted_at?: Date | null
 }
 export interface User extends AppBaseEntity {
-    first_name: string;
-    last_name: string;
-    email: string;
-    password?: string;
-    phone: string;
-    dob: Date | string;
-    gender: Gender;
-    address: string;
-    role: Role;
+  first_name: string
+  last_name: string
+  email: string
+  password?: string
+  phone: string
+  dob: Date | string
+  gender: Gender
+  address: string
+  role: Role
 }
 export interface Artist extends AppBaseEntity {
-    name: string;
-    dob: Date | string;
-    gender: Gender;
-    address: string;
-    first_release_year: number;
-    no_of_albums_released: number;
+  name: string
+  dob: Date | string
+  gender: Gender
+  address: string
+  first_release_year: number
+  no_of_albums_released: number
 }
 export interface Song extends AppBaseEntity {
-    artist_id: string;
-    title: string;
-    album_name: string;
-    genre: string;
+  artist_id: string
+  title: string
+  album_name: string
+  genre: string
 }
 //# sourceMappingURL=index.d.ts.map
